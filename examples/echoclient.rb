b69959ffb0088eb92748d56c84931555482b8b92
Aditya Godbole <aditya.a.godbole@gmail.com>
2013-08-15 12:25:43 +0530

    modify redis client to use backend from the gem

--- examples/echoclient.rb
+++ examples/echoclient.rb
@@ -1,30 +1,18 @@
-require 'gilmour/protocol'
-require 'em-hiredis'
-
-@count = 0
-def subscribe_and_send_payload(redis, message, key)
-  @count = (@count + 1) % 10
-  key = "#{key}.#{SecureRandom.hex(2)}"
-  m = @count == 0 ? 'Palmolive' : message
-  payload, sender = Gilmour::Protocol.create_request(m)
-  response_topic = "response.#{sender}"
-  redis.pubsub.subscribe(response_topic)
-  redis.publish(key, payload)
-  puts "#{key}: #{payload}"
-  response_topic
-end
+require 'securerandom'
+require 'gilmour/backends/redis'
 
 def redis_send_and_recv(message, key)
-  EM.run do
-    redis = EM::Hiredis.connect
-    response_topic = subscribe_and_send_payload(redis, message, key)
-    redis.pubsub.on(:message) do |topic, data|
-      response, code, _ = Gilmour::Protocol.parse_response(data)
-      puts response
-      redis.pubsub.unsubscribe(response_topic)
-      sleep 1
-      response_topic = subscribe_and_send_payload(redis, message, key)
+  redis = Gilmour::RedisBackend.new({})
+  redis.setup_subscribers({})
+  loop do
+    waiter = Thread.new { loop { sleep 1 } }
+    newkey = "#{key}.#{SecureRandom.hex(2)}"
+    redis.send(message, newkey) do |data, code|
+      puts "Client got response: #{code}: #{data}"
+      waiter.kill
     end
+    waiter.join
+    sleep 1
   end
 end
 
