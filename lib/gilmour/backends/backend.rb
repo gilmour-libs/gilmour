4771ef4cf68c4f3fdf89fa184eff814d0827328b
Aditya Godbole <aditya.a.godbole@gmail.com>
2013-08-15 11:00:48 +0530

    Move backends out of the base.
    Enable loading of backends only when required.
    Also, enable loading the backend to use outside
    the microservice container

---
+++ lib/gilmour/backends/backend.rb
@@ -0,0 +1,40 @@
+# encoding: utf-8
+module Gilmour
+  # Base class for loading backends
+  class Backend
+    SUPPORTED_BACKENDS = %w(amqp redis)
+    @@dir = {}
+
+    def self.implements(token)
+      @@dir[token] = self
+    end
+
+    def self.get(token)
+      @@dir[token]
+    end
+
+    # This should be implemented by the derived class
+    # subscriptions is a hash in the format -
+    # { topic => [handler1, handler2, ...],
+    #   topic2 => [handler3, handler4, ...],
+    #   ...
+    # }
+    # where handler is a hash
+    # { :handler => handler_proc,
+    #   :subscriber => subscriber_derived_class
+    # }
+    def setup_subscribers(subscriptions)
+    end
+
+    def self.load_backend(name)
+      require_relative name
+    end
+
+    def self.load_all_backends
+      SUPPORTED_BACKENDS.each do |f|
+        load_backend f
+      end
+    end
+  end
+end
+
--- lib/gilmour/backends/redis.rb
+++ lib/gilmour/backends/redis.rb
@@ -1,62 +1,61 @@
 require 'em-hiredis'
+require_relative 'backend'
 
 module Gilmour
-  module Base
-    class RedisBackend < Backend
-      implements 'redis'
+  class RedisBackend < Backend
+    implements 'redis'
 
-      attr_reader :subscriber
-      attr_reader :publisher
+    attr_reader :subscriber
+    attr_reader :publisher
 
-      def initialize(opts)
-        @subscriptions = {}
-        waiter = Thread.new { loop { sleep 1 } }
-        Thread.new do
-          EM.run do
-            @publisher = EM::Hiredis.connect
-            @subscriber = @publisher.pubsub
-            waiter.kill
-          end
-        end
-        waiter.join
-      end
-
-      def execute_handler(topic, payload, handler)
-        data, sender = Gilmour::Protocol.parse_request(payload)
-        body, code = Gilmour::Responder.new(topic, data)
-        .execute(handler)
-        send_async(body, code, sender) if code && sender
-      end
-
-      def subscribe_topic(topic)
-        method = topic.index('*') ? :psubscribe : :subscribe 
-        @subscriber.method(method).call(topic)
-      end
-
-      def pmessage_handler(key, matched_topic, payload)
-        @subscriptions[key].each do |subscription|
-          execute_handler(matched_topic, payload, subscription[:handler])
+    def initialize(opts)
+      @subscriptions = {}
+      waiter = Thread.new { loop { sleep 1 } }
+      Thread.new do
+        EM.run do
+          @publisher = EM::Hiredis.connect
+          @subscriber = @publisher.pubsub
+          waiter.kill
         end
       end
+      waiter.join
+    end
 
-      def setup_subscribers(subs)
-        @subscriptions = subs
-        EM.defer do
-          subs.keys.each { |topic| subscribe_topic(topic) }
-          @subscriber.on(:pmessage) do |key, topic, payload|
-            pmessage_handler(key, topic, payload)
-          end
-          @subscriber.on(:message) do |topic, payload|
-            pmessage_handler(topic, topic, payload)
-          end
+    def execute_handler(topic, payload, handler)
+      data, sender = Gilmour::Protocol.parse_request(payload)
+      body, code = Gilmour::Responder.new(topic, data)
+      .execute(handler)
+      send_async(body, code, sender) if code && sender
+    end
+
+    def subscribe_topic(topic)
+      method = topic.index('*') ? :psubscribe : :subscribe
+      @subscriber.method(method).call(topic)
+    end
+
+    def pmessage_handler(key, matched_topic, payload)
+      @subscriptions[key].each do |subscription|
+        execute_handler(matched_topic, payload, subscription[:handler])
+      end
+    end
+
+    def setup_subscribers(subs)
+      @subscriptions = subs
+      EM.defer do
+        subs.keys.each { |topic| subscribe_topic(topic) }
+        @subscriber.on(:pmessage) do |key, topic, payload|
+          pmessage_handler(key, topic, payload)
+        end
+        @subscriber.on(:message) do |topic, payload|
+          pmessage_handler(topic, topic, payload)
         end
       end
+    end
 
-      def send_async(data, code, destination)
-        payload, _ = Gilmour::Protocol.create_request(data, code)
-        key = "response.#{destination}"
-        @publisher.publish(key, payload)
-      end
+    def send_async(data, code, destination)
+      payload, _ = Gilmour::Protocol.create_request(data, code)
+      key = "response.#{destination}"
+      @publisher.publish(key, payload)
     end
   end
 end
