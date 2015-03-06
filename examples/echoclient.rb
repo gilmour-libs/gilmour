33b2976fcda5989c249f5c83abe2a72695b7ff2a
aag <code.aa@gdbl.me>
2015-03-06 18:23:00 +0530

    Updates examples. Bump version

--- examples/echoclient.rb
+++ examples/echoclient.rb
@@ -4,13 +4,15 @@ require 'gilmour/backends/redis'
 def redis_send_and_recv(message, key)
   redis = Gilmour::RedisBackend.new({})
   redis.setup_subscribers({})
+  count = 0
   loop do
     waiter = Thread.new { loop { sleep 1 } }
     newkey = "#{key}.#{SecureRandom.hex(2)}"
-    redis.publish(message, newkey) do |data, code|
+    redis.publish(count, newkey) do |data, code|
       puts "Client got response: #{code}: #{data}"
       waiter.kill
     end
+    count = count + 1
     waiter.join
     sleep 1
   end
