c3b19cce6681b6a282a6ce6bf1efe916dd38d07f
Piyush Verma <github@piyushverma.net>
2015-06-25 02:13:08 +0530

    uncomment test cases

--- test/spec/test_subscriber_redis.rb
+++ test/spec/test_subscriber_redis.rb
@@ -67,274 +67,274 @@ describe 'TestSubscriber' do
       end
     end
 
-    #context 'Non Fork can add dynamic listeners', :fork_dynamic do
-    #  Given(:ping_opts) do
-    #    redis_ping_options
-    #  end
+    context 'Non Fork can add dynamic listeners', :fork_dynamic do
+      Given(:ping_opts) do
+        redis_ping_options
+      end
 
-    #  When(:sub) do
-    #    Gilmour::RedisBackend.new({})
-    #  end
-    #  When(:code) do
-    #    waiter = Waiter.new
-    #    dynamicaly_subscribed = 0
+      When(:sub) do
+        Gilmour::RedisBackend.new({})
+      end
+      When(:code) do
+        waiter = Waiter.new
+        dynamicaly_subscribed = 0
 
-    #    sub.publish(1, TestSubscriber::ReListenTopic) do |d, c|
-    #      sub.confirm_subscriber("test.world") do |present|
-    #        dynamicaly_subscribed = present
-    #        waiter.signal
-    #      end
-    #    end
+        sub.publish(1, TestSubscriber::ReListenTopic) do |d, c|
+          sub.confirm_subscriber("test.world") do |present|
+            dynamicaly_subscribed = present
+            waiter.signal
+          end
+        end
 
-    #    waiter.wait(5)
-    #    dynamicaly_subscribed
-    #  end
-    #  Then do
-    #    code.should be == true
-    #  end
-    #end
+        waiter.wait(5)
+        dynamicaly_subscribed
+      end
+      Then do
+        code.should be == true
+      end
+    end
 
-    #context 'Publisher side timeout' do
-    #  Given(:ping_opts) do
-    #    redis_ping_options
-    #  end
+    context 'Publisher side timeout' do
+      Given(:ping_opts) do
+        redis_ping_options
+      end
 
-    #  When(:sub) do
-    #    Gilmour::RedisBackend.new({})
-    #  end
-    #  When(:code) do
-    #    code = nil
-    #    waiter_code = Waiter.new
+      When(:sub) do
+        Gilmour::RedisBackend.new({})
+      end
+      When(:code) do
+        code = nil
+        waiter_code = Waiter.new
 
-    #    sub.publish(3, TestSubscriber::TimeoutTopic, {:timeout => 1}) do |d, c|
-    #      code = c
-    #      waiter_code.signal
-    #    end
+        sub.publish(3, TestSubscriber::TimeoutTopic, {:timeout => 1}) do |d, c|
+          code = c
+          waiter_code.signal
+        end
 
-    #    waiter_code.wait(5)
-    #    code
-    #  end
-    #  Then do
-    #    code.should be == 499
-    #  end
-    #end
+        waiter_code.wait(5)
+        code
+      end
+      Then do
+        code.should be == 499
+      end
+    end
 
-    #context 'Handler sleeps longer than the Timeout' do
-    #  Given(:ping_opts) do
-    #    redis_ping_options
-    #  end
+    context 'Handler sleeps longer than the Timeout' do
+      Given(:ping_opts) do
+        redis_ping_options
+      end
 
-    #  When(:sub) do
-    #    Gilmour::RedisBackend.new({})
-    #  end
-    #  When(:code) do
-    #    waiter_error = Waiter.new
-    #    waiter_code = Waiter.new
-    #    code = nil
+      When(:sub) do
+        Gilmour::RedisBackend.new({})
+      end
+      When(:code) do
+        waiter_error = Waiter.new
+        waiter_code = Waiter.new
+        code = nil
 
-    #    error_listener_proc = sub.add_listener Gilmour::ErrorChannel do
-    #      waiter_error.signal
-    #    end
+        error_listener_proc = sub.add_listener Gilmour::ErrorChannel do
+          waiter_error.signal
+        end
 
-    #    sub.publish(3, TestSubscriber::TimeoutTopic) do |d, c|
-    #      code = c
-    #      waiter_code.signal
-    #    end
+        sub.publish(3, TestSubscriber::TimeoutTopic) do |d, c|
+          code = c
+          waiter_code.signal
+        end
 
-    #    waiter_code.wait(5)
-    #    waiter_error.wait(5)
+        waiter_code.wait(5)
+        waiter_error.wait(5)
 
-    #    sub.remove_listener Gilmour::ErrorChannel, error_listener_proc
+        sub.remove_listener Gilmour::ErrorChannel, error_listener_proc
 
-    #    code
-    #  end
-    #  Then do
-    #    code.should be == 504
-    #  end
-    #end
+        code
+      end
+      Then do
+        code.should be == 504
+      end
+    end
 
-    #context 'Handler sleeps just enough to survive the timeout' do
-    #  Given(:ping_opts) do
-    #    redis_ping_options
-    #  end
+    context 'Handler sleeps just enough to survive the timeout' do
+      Given(:ping_opts) do
+        redis_ping_options
+      end
 
-    #  When(:sub) do
-    #    Gilmour::RedisBackend.new({})
-    #  end
-    #  When(:code) do
-    #    waiter = Waiter.new
-    #    code = nil
+      When(:sub) do
+        Gilmour::RedisBackend.new({})
+      end
+      When(:code) do
+        waiter = Waiter.new
+        code = nil
 
-    #    sub.publish(1, TestSubscriber::TimeoutTopic) do |d, c|
-    #      code = c
-    #      waiter.signal
-    #    end
+        sub.publish(1, TestSubscriber::TimeoutTopic) do |d, c|
+          code = c
+          waiter.signal
+        end
 
-    #    waiter.wait(5)
-    #    code
-    #  end
-    #  Then do
-    #    code.should be == 200
-    #  end
-    #end
+        waiter.wait(5)
+        code
+      end
+      Then do
+        code.should be == 200
+      end
+    end
 
-    #context 'Receive messages' do
-    #  context 'Receive a message' do
-    #    Given(:ping_opts) { redis_ping_options }
-    #    When do
-    #      @data = @topic = nil
-    #      waiter = install_test_subscriber(TestServiceBase)
-    #      redis_publish_async(connection_opts,
-    #                          ping_opts[:message],
-    #                          TestSubscriber::Topic)
-    #      waiter.wait(5)
-    #    end
-    #    Then do
-    #      @data.should be == ping_opts[:message]
-    #      @topic.should be == TestSubscriber::Topic
-    #    end
-    #    And { expect(EM.reactor_thread.alive?).to be_truthy }
-    #  end
+    context 'Receive messages' do
+      context 'Receive a message' do
+        Given(:ping_opts) { redis_ping_options }
+        When do
+          @data = @topic = nil
+          waiter = install_test_subscriber(TestServiceBase)
+          redis_publish_async(connection_opts,
+                              ping_opts[:message],
+                              TestSubscriber::Topic)
+          waiter.wait(5)
+        end
+        Then do
+          @data.should be == ping_opts[:message]
+          @topic.should be == TestSubscriber::Topic
+        end
+        And { expect(EM.reactor_thread.alive?).to be_truthy }
+      end
 
-    #  context 'Recieve a message on a wildcard key' do
-    #    Given(:wildcard_opts) { redis_wildcard_options }
-    #    When do
-    #      @data = @topic = nil
-    #      waiter = install_test_subscriber(TestServiceBase)
-    #      redis_publish_async(connection_opts,
-    #                          wildcard_opts[:message],
-    #                          wildcard_opts[:topic])
-    #      waiter.wait(5)
-    #    end
-    #    Then { @data.should == wildcard_opts[:message] }
-    #    And  { @topic.should == wildcard_opts[:topic] }
-    #  end
-    #end
+      context 'Recieve a message on a wildcard key' do
+        Given(:wildcard_opts) { redis_wildcard_options }
+        When do
+          @data = @topic = nil
+          waiter = install_test_subscriber(TestServiceBase)
+          redis_publish_async(connection_opts,
+                              wildcard_opts[:message],
+                              wildcard_opts[:topic])
+          waiter.wait(5)
+        end
+        Then { @data.should == wildcard_opts[:message] }
+        And  { @topic.should == wildcard_opts[:topic] }
+      end
+    end
 
-    #context 'Publish sans subscriber timeout' do
-    #  Given(:ping_opts) do
-    #    redis_ping_options
-    #  end
+    context 'Publish sans subscriber timeout' do
+      Given(:ping_opts) do
+        redis_ping_options
+      end
 
-    #  When(:sub) do
-    #    Gilmour::RedisBackend.new({})
-    #  end
-    #  When(:response) do
-    #    waiter = Waiter.new
-    #    data = code = nil
-    #    sub.publish(ping_opts[:message], "hello.world") do |d, c|
-    #      data = d
-    #      code = c
-    #      waiter.signal
-    #    end
-    #    waiter.wait(5)
-    #    [data, code]
-    #  end
-    #  Then do
-    #    data, code = response
-    #    data.should be == nil
-    #    code.should be == nil
-    #  end
+      When(:sub) do
+        Gilmour::RedisBackend.new({})
+      end
+      When(:response) do
+        waiter = Waiter.new
+        data = code = nil
+        sub.publish(ping_opts[:message], "hello.world") do |d, c|
+          data = d
+          code = c
+          waiter.signal
+        end
+        waiter.wait(5)
+        [data, code]
+      end
+      Then do
+        data, code = response
+        data.should be == nil
+        code.should be == nil
+      end
 
-    #end
+    end
 
-    #context 'Send and receive a message' do
-    #  Given(:ping_opts) { redis_ping_options }
-    #  When(:sub) do
-    #    Gilmour::RedisBackend.new({})
-    #  end
-    #  When(:response) do
-    #    waiter = Waiter.new
-    #    data = code = nil
-    #    sub.publish(ping_opts[:message], TestSubscriber::Topic, { confirm_subscriber: true }) do |d, c|
-    #      data = d
-    #      code = c
-    #      waiter.signal
-    #    end
-    #    waiter.wait(5)
-    #    [data, code]
-    #  end
-    #  Then do
-    #    data, code = response
-    #    data.should be == ping_opts[:response]
-    #    code.should be == 200
-    #  end
-    #end
+    context 'Send and receive a message' do
+      Given(:ping_opts) { redis_ping_options }
+      When(:sub) do
+        Gilmour::RedisBackend.new({})
+      end
+      When(:response) do
+        waiter = Waiter.new
+        data = code = nil
+        sub.publish(ping_opts[:message], TestSubscriber::Topic, { confirm_subscriber: true }) do |d, c|
+          data = d
+          code = c
+          waiter.signal
+        end
+        waiter.wait(5)
+        [data, code]
+      end
+      Then do
+        data, code = response
+        data.should be == ping_opts[:response]
+        code.should be == 200
+      end
+    end
 
-    #context 'Send once, Receive twice' do
-    #  Given(:ping_opts) { redis_ping_options }
-    #  When(:sub) do
-    #    Gilmour::RedisBackend.new({})
-    #  end
-    #  When (:response) do
-    #    waiter = Waiter.new
+    context 'Send once, Receive twice' do
+      Given(:ping_opts) { redis_ping_options }
+      When(:sub) do
+        Gilmour::RedisBackend.new({})
+      end
+      When (:response) do
+        waiter = Waiter.new
 
-    #    actual_ret = []
+        actual_ret = []
 
-    #    group_proc = sub.add_listener TestSubscriber::GroupReturn do
-    #      actual_ret.push(request.body)
-    #      waiter.signal if actual_ret.length == 4
-    #    end
+        group_proc = sub.add_listener TestSubscriber::GroupReturn do
+          actual_ret.push(request.body)
+          waiter.signal if actual_ret.length == 4
+        end
 
-    #    sub.publish(ping_opts[:message], TestSubscriber::GroupTopic)
-    #    waiter.wait(5)
+        sub.publish(ping_opts[:message], TestSubscriber::GroupTopic)
+        waiter.wait(5)
 
-    #    sub.remove_listener TestSubscriber::GroupReturn, group_proc
-    #    actual_ret
-    #  end
-    #  Then do
-    #    response.select { |e| e == ping_opts[:message] }.size.should == 2
-    #    response.select { |e| e == "2" }.size.should == 2
-    #  end
-    #end
+        sub.remove_listener TestSubscriber::GroupReturn, group_proc
+        actual_ret
+      end
+      Then do
+        response.select { |e| e == ping_opts[:message] }.size.should == 2
+        response.select { |e| e == "2" }.size.should == 2
+      end
+    end
 
-    #context 'Send once, Receive Once' do
-    #  Given(:ping_opts) { redis_ping_options }
-    #  When(:sub) do
-    #    Gilmour::RedisBackend.new({})
-    #  end
-    #  When (:response) do
-    #    waiter = Waiter.new
-    #    actual_ret = []
+    context 'Send once, Receive Once' do
+      Given(:ping_opts) { redis_ping_options }
+      When(:sub) do
+        Gilmour::RedisBackend.new({})
+      end
+      When (:response) do
+        waiter = Waiter.new
+        actual_ret = []
 
-    #    group_proc = sub.add_listener TestSubscriber::GroupReturn do
-    #      actual_ret.push(request.body)
-    #      waiter.signal if actual_ret.length == 1
-    #    end
+        group_proc = sub.add_listener TestSubscriber::GroupReturn do
+          actual_ret.push(request.body)
+          waiter.signal if actual_ret.length == 1
+        end
 
-    #    sub.publish(ping_opts[:message], TestSubscriber::ExclusiveTopic)
+        sub.publish(ping_opts[:message], TestSubscriber::ExclusiveTopic)
 
-    #    waiter.wait(5)
-    #    sub.remove_listener TestSubscriber::GroupReturn, group_proc
-    #    actual_ret
-    #  end
-    #  Then do
-    #    response.size.should == 1
-    #  end
-    #end
+        waiter.wait(5)
+        sub.remove_listener TestSubscriber::GroupReturn, group_proc
+        actual_ret
+      end
+      Then do
+        response.size.should == 1
+      end
+    end
 
-    #context 'Send message from subscriber' do
-    #  Given(:ping_opts) { redis_ping_options }
-    #  When(:sub) do
-    #    Gilmour::RedisBackend.new({})
-    #  end
-    #  When (:response) do
-    #    data = code = nil
-    #    waiter = Waiter.new
-    #    sub.publish(ping_opts[:message], 'test.republish') do |d, c|
-    #      data = d
-    #      code = c
-    #      waiter.signal
-    #    end
-    #    waiter.wait(5)
-    #    [data, code]
-    #  end
-    #  Then do
-    #    data, code = response
-    #    data.should be == ping_opts[:response]
-    #    code.should be == 200
-    #  end
-    #  And { expect(EM.reactor_thread.alive?).to be_truthy }
-    #end
+    context 'Send message from subscriber' do
+      Given(:ping_opts) { redis_ping_options }
+      When(:sub) do
+        Gilmour::RedisBackend.new({})
+      end
+      When (:response) do
+        data = code = nil
+        waiter = Waiter.new
+        sub.publish(ping_opts[:message], 'test.republish') do |d, c|
+          data = d
+          code = c
+          waiter.signal
+        end
+        waiter.wait(5)
+        [data, code]
+      end
+      Then do
+        data, code = response
+        data.should be == ping_opts[:response]
+        code.should be == 200
+      end
+      And { expect(EM.reactor_thread.alive?).to be_truthy }
+    end
   end
 end
