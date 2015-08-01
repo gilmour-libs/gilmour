2273d9079ff079785e20c26b4d5fb427780396da
Piyush Verma <github@piyushverma.net>
2015-08-01 22:55:18 +0530

    Add test cases for WaitGroup

--- test/spec/helpers/common.rb
+++ test/spec/helpers/common.rb
@@ -1,26 +1,6 @@
+require '../lib/gilmour/waiter'
 
-class Waiter
-  def initialize
-    @done = false
-    @waiter_m = Mutex.new
-    @waiter_c = ConditionVariable.new
-  end
-
-  def synchronize(&blk)
-    @waiter_m.synchronize(&blk)
-  end
-
-  def signal
-    synchronize do
-      @done = true
-      @waiter_c.signal
-    end
-  end
-
-  def wait(timeout=nil)
-    synchronize { @waiter_c.wait(@waiter_m, timeout) unless @done }
-  end
-end
+Waiter = Gilmour::Waiter
 
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
